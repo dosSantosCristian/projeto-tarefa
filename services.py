2821ec924ad9e53d9ead78e8b9dbfdfd1c768c5b
Cristian <criistiant@yahoo.com.br>
2026-09-16 06:29:36 -0300

    atualização do deletar_usuario

--- services.py
+++ services.py
@@ -13,55 +13,89 @@ def listar_usuarios():
 def buscar_usuario(id: int):
     db = SessionLocal()
 
-    usuario = db.query(UsuarioDB).filter(UsuarioDB.id == id).first()
+    try:
+        usuario = (
+            db.query(UsuarioDB)
+            .filter(UsuarioDB.id == id)
+            .first()
+        )
 
-    db.close()
+        return usuario
 
-    return usuario
+    except Exception:
+        db.rollback()
+        raise
+
+    finally:
+        db.close() 
 
 def deletar_usuario(id: int):
     db = SessionLocal()
 
-    usuario = db.query(UsuarioDB).filter(UsuarioDB.id == id).first()
+    try:
+        usuario = (
+            db.query(UsuarioDB)
+            .filter(UsuarioDB.id == id)
+            .first()
+        )
 
-    if usuario is None:
+        if usuario is None:
+            return None
+
+        db.delete(usuario)
+        db.commit()
+
+        return usuario
+
+    except Exception:
+        db.rollback()
+        raise
+
+    finally:
         db.close()
-        return None
-
-    db.delete(usuario)
-    db.commit()
-
-    db.close()
-
-    return usuario
 
 def atualizar_usuario(id: int, nome: str):
     db = SessionLocal()
 
-    usuario = db.query(UsuarioDB).filter(UsuarioDB.id == id).first()
+    try:
+        usuario = (
+            db.query(UsuarioDB)
+            .filter(UsuarioDB.id == id)
+            .first()
+        )
 
-    if usuario is None:
+        if usuario is None:
+            return None
+
+        usuario.nome = nome
+
+        db.commit()
+        db.refresh(usuario)
+
+        return usuario
+
+    except Exception:
+        db.rollback
+        raise
+
+    finally:
         db.close()
-        return None
-
-    usuario.nome = nome
-
-    db.commit()
-    db.refresh(usuario)
-
-    db.close()
-
-    return usuario
 
 def criar_usuario(nome: str):
     db = SessionLocal()
 
-    novo_usuario = UsuarioDB(nome=nome)
+    try:
+        novo_usuario = UsuarioDB(nome=nome)
 
-    db.add(novo_usuario)
-    db.commit()
-    db.refresh(novo_usuario)
+        db.add(novo_usuario)
+        db.commit()
+        db.refresh(novo_usuario)
 
-    db.close()
+        return novo_usuario
 
-    return novo_usuario
+    except Exception:
+        db.rollback()
+        raise
+
+    finally:
+        db.close()
